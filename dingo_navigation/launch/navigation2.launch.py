16ad2db4626b3f2681619d48d900920695e2f2ae
joe28965 <joeptool@gmail.com>
2022-02-02 12:03:58 +0100

    split nav2 and rviz

--- dingo_navigation/launch/navigation2.launch.py
+++ dingo_navigation/launch/navigation2.launch.py
@@ -22,21 +22,17 @@ from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     map_dir = LaunchConfiguration('map', default=os.path.join(get_package_share_directory('dingo_navigation'),
                                   'map', 'map.yaml'))
 
-    param_file_name = 'Dingo_Nav_params.yaml'
+    param_file_name = 'dingo_d_nav2_params.yaml'
     param_dir = LaunchConfiguration('params_file', default=os.path.join(get_package_share_directory('dingo_navigation'),
                                     'params', param_file_name))
 
     nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
-
-    rviz_config_dir = os.path.join(get_package_share_directory('dingo_navigation'),
-                                   'rviz', 'dingo_nav2.rviz')
 
     return LaunchDescription([
         DeclareLaunchArgument('map', default_value=map_dir,
@@ -53,13 +49,5 @@ def generate_launch_description():
                 'map': map_dir,
                 'use_sim_time': use_sim_time,
                 'params_file': param_dir}.items(),
-        ),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+        )
     ])
